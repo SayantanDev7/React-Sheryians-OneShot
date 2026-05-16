6d90c58b508bb91d76392190a9577be58608f31e
Sayantan699 <sayantanhalder43@gmail.com>
2026-05-16 18:25:49 +0530

    converted the Navbar component's external CSS to Tailwind CSS classes and removed the redundant Navbar.css file.

--- Basics Revision/src/components/Navbar.jsx
+++ Basics Revision/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import './Navbar.css'
+
 const Navbar = () => {
   return (
-    <nav className='nav'>
-        <ul className='nav-links'>
-            <li key="home" className='nav-link'>Home</li>
-            <li key= "about" className='nav-link'>About</li>
-            <li key="contact" className='nav-link'>Contact</li>
+    <nav className="flex gap-5 justify-center items-center bg-[#807b7b] p-2.5">
+        <ul className="list-none flex gap-5">
+            <li key="home" className="cursor-pointer bg-[#b3c4e0] text-black p-2.5 rounded-[5px] font-sans text-center hover:bg-[#96aad1] hover:text-white">Home</li>
+            <li key="about" className="cursor-pointer bg-[#b3c4e0] text-black p-2.5 rounded-[5px] font-sans text-center hover:bg-[#96aad1] hover:text-white">About</li>
+            <li key="contact" className="cursor-pointer bg-[#b3c4e0] text-black p-2.5 rounded-[5px] font-sans text-center hover:bg-[#96aad1] hover:text-white">Contact</li>
         </ul>
     </nav>
   )
 }
 
-export default Navbar   
+export default Navbar
