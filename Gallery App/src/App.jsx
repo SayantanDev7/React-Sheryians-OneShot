4954247f17c60251a6d0b76006ce81b82392763a
Sayantan699 <sayantanhalder43@gmail.com>
2026-05-27 12:31:12 +0530

    Added infinite scrolling on clicking load more btn

--- Gallery App/src/App.jsx
+++ Gallery App/src/App.jsx
@@ -5,13 +5,16 @@ import { useEffect,useState} from 'react'
 const App = () => {
 
   const [data,setData] = useState([]);
+  const[currpage,setCurrpage] = useState(1);
   const getdata = async () =>{
-    const info = await axios.get('https://picsum.photos/v2/list?page=2&limit=10')
-    setData(info.data);
+    const info = await axios.get(`https://picsum.photos/v2/list?page=${currpage}&limit=10`)
+    setData((prevdata) =>{
+      return [...prevdata, ...info.data] //instead of completely replacing data it adds new data to the previous data
+    });
   }
   useEffect(() =>{
     getdata();
-  }, [])
+  }, [currpage]) //mounts data when currpage changes
   return (
     <>
     <GalleryHeader />
@@ -26,6 +29,8 @@ const App = () => {
         <PhotoCard key={item.id} title={item.author} url={item.download_url} id={item.id} />
       ))/* End of mapped data */}
     </div>
+    <button onClick={() => setCurrpage(currpage + 1)} className="text-white bg-purple-500 hover:bg-purple-700 transition-colors duration-300 ease-in-out font-bold py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 flex justify-center items-center"
+    >Load More</button>
     </>
   )
 }
